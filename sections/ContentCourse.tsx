c7eea68a72e71df134b7659c4b456cdc2337843b
Megas Melquisedeque <mdn.melqui@gmail.com>
2024-07-12 18:52:37 -0300

    feat: Contents

--- sections/ContentCourse.tsx
+++ sections/ContentCourse.tsx
@@ -10,8 +10,8 @@ interface Props {
 const st = "border border-red-500";
 export default function Section({ name }: Props) {
   return (
-    <div class={`w-full h-full ${st} bg-primary`}>
-      <p>{name?.capy || "Capy"}</p>
+    <div class={`w-full h-full ${st}`}>
+      <SideBarCourse capy={name?.capy} />
     </div>
   );
 }
