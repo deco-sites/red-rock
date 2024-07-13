9de03f93e57bae3e3401d9b3b60bd0a071923b20
EduardoBalbinot <eduardo.balbinot.dev@gmail.com>
2024-07-13 02:56:36 -0300

    testimonials carla

--- sections/Testimonials.tsx
+++ sections/Testimonials.tsx
@@ -1,232 +1,64 @@
-import type { ImageWidget } from "apps/admin/widgets.ts";
+import { ImageWidget } from "apps/admin/widgets.ts";
 import Image from "apps/website/components/Image.tsx";
-import Icon from "../components/ui/Icon.tsx";
-import Slider from "../components/ui/Slider.tsx";
-import { useId } from "../sdk/useId.ts";
 
-/**
- * @titleBy alt
- */
-export interface Testimonial {
-  content?: {
-    description?: string;
-    avatar?: ImageWidget;
-    /** @description Image's alt text */
-    alt?: string;
-    name?: string;
-    position?: string;
-  };
+export interface Feature {
+  label: string;
+}
+
+export interface Card {
+  icone: ImageWidget;
+  cardTitle: string;
+  featureList: Feature[];
+  price: string;
+  buttonText: "FREE" | "GET STARTED";
 }
 
 export interface Props {
+  /**
+   
+@description The description of name.*/
   title?: string;
-  slides?: Testimonial[];
-  /**
-   * @title Show arrows
-   * @description show arrows to navigate through the images
-   */
-  arrows?: boolean;
-  /**
-   * @title Show dots
-   * @description show dots to navigate through the images
-   */
-  dots?: boolean;
-  /**
-   * @title Autoplay interval
-   * @description time (in seconds) to start the carousel autoplay
-   */
-  interval?: number;
+  subtitle?: string;
+  itemCard: Card[];
 }
-
-const DEFAULT_PROPS = {
-  title: "This is where you'll put your customer testimonials",
-  slides: [
-    {
-      content: {
-        description:
-          "Showcase customer feedback that emphasizes your product or service's key features and addresses prospective clients' concerns. Display endorsements from customer groups that mirror your target audience.",
-        avatar:
-          "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/1527/7286de42-e9c5-4fcb-ae8b-b992eea4b78e",
-        alt: "Avatar",
-        name: "Name Surname",
-        position: "Position, Company name",
-      },
-    },
-    {
-      content: {
-        description:
-          "Showcase customer feedback that emphasizes your product or service's key features and addresses prospective clients' concerns. Display endorsements from customer groups that mirror your target audience.",
-        avatar:
-          "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/1527/7286de42-e9c5-4fcb-ae8b-b992eea4b78e",
-        alt: "Avatar",
-        name: "Name Surname",
-        position: "Position, Company name",
-      },
-    },
-    {
-      content: {
-        description:
-          "Showcase customer feedback that emphasizes your product or service's key features and addresses prospective clients' concerns. Display endorsements from customer groups that mirror your target audience.",
-        avatar:
-          "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/1527/7286de42-e9c5-4fcb-ae8b-b992eea4b78e",
-        alt: "Avatar",
-        name: "Name Surname",
-        position: "Position, Company name",
-      },
-    },
-    {
-      content: {
-        description:
-          "Showcase customer feedback that emphasizes your product or service's key features and addresses prospective clients' concerns. Display endorsements from customer groups that mirror your target audience.",
-        avatar:
-          "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/1527/7286de42-e9c5-4fcb-ae8b-b992eea4b78e",
-        alt: "Avatar",
-        name: "Name Surname",
-        position: "Position, Company name",
-      },
-    },
-    {
-      content: {
-        description:
-          "Showcase customer feedback that emphasizes your product or service's key features and addresses prospective clients' concerns. Display endorsements from customer groups that mirror your target audience.",
-        avatar:
-          "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/1527/7286de42-e9c5-4fcb-ae8b-b992eea4b78e",
-        alt: "Avatar",
-        name: "Name Surname",
-        position: "Position, Company name",
-      },
-    },
-  ],
-};
-
-function SliderItem(
-  { slide, id }: { slide: Testimonial; id: string },
-) {
-  const {
-    content,
-  } = slide;
-
+export default function Section({ title, subtitle, itemCard }: Props) {
   return (
-    <div
-      id={id}
-      class="relative overflow-y-hidden w-full min-h-[292px]"
-    >
-      <div class="flex flex-col justify-center gap-16 p-8 border border-base-content rounded-2xl h-full max-w-[600px]">
-        <p class="text-lg">{content?.description}</p>
-        <div class="flex items-center gap-5">
-          <Image
-            class="object-cover w-14 h-14 rounded-full"
-            alt={content?.alt}
-            src={content?.avatar || ""}
-            width={56}
-            height={56}
-          />
-          <div class="flex flex-col">
-            <p class="font-semibold text-base">{content?.name}</p>
-            <p class="text-base">{content?.position}</p>
-          </div>
+    <div class="bg-accent p-24">
+      <div class="md:w-[1300px] mx-auto flex flex-col items-center　">
+        <p class="text-4xl text-secondary font-bold text-center">{title}</p>
+        <p class="text-xl text-secondary font-medium pt-5 text-center">
+          {subtitle}
+        </p>
+        <div class="flex pt-10 gap-20 justify-center">
+          {itemCard.map((item) => (
+            <div class=" border border-primary rounded-xl p-8 bg-secondary w-11/12 md:w-72 flex flex-col items-center">
+              <Image
+                width={80}
+                height={80}
+                class="w-[80px] h-[80px] object-cover mx-auto"
+                src={item.icone}
+                alt={item.cardTitle}
+                decoding="async"
+                loading="lazy"
+              />
+              <h3 class="text-primary text-2xl font-semibold text-center pt-4">
+                {item.cardTitle}
+              </h3>
+              <ul class="w-full pt-5">
+                {item.featureList.map((itemList) => (
+                  <li class="text-neutral border-b-2 last:border-0 border-accent leading-loose w-full">
+                    {itemList.label}
+                  </li>
+                ))}
+              </ul>
+              <p class="text-right text-primary text-4xl font-semibold pt-5">
+                {item.price}
+              </p>
+              <a class="btn btn-primary mt-5">{item.buttonText}</a>
+            </div>
+          ))}
         </div>
       </div>
     </div>
   );
 }
-
-function Dots({ slides, interval = 0 }: Props) {
-  return (
-    <>
-      <style
-        dangerouslySetInnerHTML={{
-          __html: `
-          @property --dot-progress {
-            syntax: '<percentage>';
-            inherits: false;
-            initial-value: 0%;
-          }
-          `,
-        }}
-      />
-      <ul class="carousel col-span-full gap-3 z-10">
-        {slides?.map((_, index) => (
-          <li class="carousel-item">
-            <Slider.Dot index={index}>
-              <div class="py-5">
-                <div
-                  class="w-2 h-2 rounded-full group-disabled:animate-progress dot"
-                  style={{ animationDuration: `${interval}s` }}
-                />
-              </div>
-            </Slider.Dot>
-          </li>
-        ))}
-      </ul>
-    </>
-  );
-}
-
-function Buttons() {
-  return (
-    <div class="flex gap-4">
-      <div class="flex items-center justify-center z-10 col-start-1 row-start-2">
-        <Slider.PrevButton class="flex items-center justify-center btn-circle border border-base-content">
-          <Icon
-            class="text-base-content"
-            size={24}
-            id="ArrowRight"
-            strokeWidth={3}
-          />
-        </Slider.PrevButton>
-      </div>
-      <div class="flex items-center justify-center z-10 col-start-3 row-start-2">
-        <Slider.NextButton class="flex items-center justify-center btn-circle border border-base-content">
-          <Icon
-            class="text-base-content"
-            size={24}
-            id="ArrowLeft"
-            strokeWidth={3}
-          />
-        </Slider.NextButton>
-      </div>
-    </div>
-  );
-}
-
-function Carousel(props: Props) {
-  const id = useId();
-  const { title, slides, interval } = { ...DEFAULT_PROPS, ...props };
-
-  return (
-    <div
-      id={id}
-      class="min-h-min flex flex-col lg:container md:max-w-6xl lg:mx-auto mx-4 py-12 lg:py-28"
-    >
-      <h2 class="text-4xl leading-snug lg:w-1/2 pb-12 lg:pb-16">
-        {title}
-      </h2>
-      <Slider
-        class="carousel carousel-center w-full col-span-full row-span-full gap-6"
-        rootId={id}
-        interval={interval && interval * 1e3}
-        infinite
-      >
-        {slides?.map((slide, index) => (
-          <Slider.Item
-            index={index}
-            class="carousel-item max-w-[600px] w-full"
-          >
-            <SliderItem
-              slide={slide}
-              id={`${id}::${index}`}
-            />
-          </Slider.Item>
-        ))}
-      </Slider>
-
-      <div class="flex justify-between pt-8 lg:px-16">
-        {props.dots && <Dots slides={slides} interval={interval} />}{" "}
-        {props.arrows && <Buttons />}
-      </div>
-    </div>
-  );
-}
-
-export default Carousel;
