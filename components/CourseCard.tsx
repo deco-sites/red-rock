9800f3297eebccb79236d9e19b1f55c3e3f51e69
Megas Melquisedeque <mdn.melqui@gmail.com>
2024-07-13 23:41:53 -0300

    fix: ImageWidget

--- components/CourseCard.tsx
+++ components/CourseCard.tsx
@@ -1,8 +1,9 @@
 import Image from "apps/website/components/Image.tsx";
+import { ImageWidget } from "apps/admin/widgets.ts";
 
 export interface ICourseCard {
   courseTitle?: string;
-  courseImage?: string;
+  courseImage?: ImageWidget;
   link?: string;
 }
 
